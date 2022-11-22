6dd252d1b3c23a10d63ae1047cc76664e37d8752
edualt <eduardosebastianhdz503@gmail.com>
2022-11-21 18:43:31 -0600

    feat(main): create and implement main widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:random_word/widgets/home.dart';
 
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const title = "Random Word";
+
+    return MaterialApp(
+      title: title,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeWidget(title: title),
+    );
+  }
+}
